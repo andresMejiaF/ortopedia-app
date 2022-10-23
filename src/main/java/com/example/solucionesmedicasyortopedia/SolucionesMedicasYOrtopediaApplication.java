9d7f24d7ddf3d91d47cd301f859fab127a0d5ecf
unknown <andressantiagomejiafigueroa@gmail.com>
2022-10-23 12:01:41 -0500

    iniciando nuevo proyecto

---
+++ src/main/java/com/example/solucionesmedicasyortopedia/SolucionesMedicasYOrtopediaApplication.java
@@ -0,0 +1,13 @@
+package com.example.solucionesmedicasyortopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolucionesMedicasYOrtopediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolucionesMedicasYOrtopediaApplication.class, args);
+    }
+
+}
